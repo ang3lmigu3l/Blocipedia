b0baf91ead4d25585b43713ecb13fee812c213ec
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-04-14 00:12:03 -0700

    adds wiki Scope

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,5 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   scope :sort_by_newest, -> { order('created_at DESC') }
+  scope :visible_to, -> (user) { user ? all : where(public: true)}
 end
