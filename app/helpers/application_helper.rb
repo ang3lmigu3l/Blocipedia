f2bdc640fb12d86c9cceffa5672720e521bbf818
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-04-14 02:21:36 -0700

    fix nav links according to account type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def user_not_admin
+    current_user && (current_user.standard? || current_user.premium?)
+  end
+
+  def user_premium_or_admin
+    current_user && (current_user.premium? || current_user.admin?)
+  end
 end
